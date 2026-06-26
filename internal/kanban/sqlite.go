fe0ca4cf32d8148ecf9aa47eeedfcb5b0064be5c
Saswata Gorai <13601653+1saswata@users.noreply.github.com>
2026-06-26 14:26:11 +0530

    feat: define sqlitestore methods

--- internal/kanban/sqlite.go
+++ internal/kanban/sqlite.go
@@ -3,6 +3,7 @@ package kanban
 import (
 	"context"
 	"database/sql"
+	"time"
 
 	"github.com/google/uuid"
 	_ "github.com/mattn/go-sqlite3"
@@ -35,7 +36,7 @@ func NewSQLiteStore(dbPath string) (*SQLiteStore, error) {
 func (ss *SQLiteStore) GetTasks(ctx context.Context) ([]Task, error) {
 	sqlString := `SELECT id, title,	description, status, created_at, updated_at
 		FROM tasks;`
-	rows, err := ss.db.Query(sqlString)
+	rows, err := ss.db.QueryContext(ctx, sqlString)
 	if err != nil {
 		return nil, err
 	}
@@ -54,13 +55,29 @@ func (ss *SQLiteStore) GetTasks(ctx context.Context) ([]Task, error) {
 }
 
 func (ss *SQLiteStore) CreateTask(ctx context.Context, t Task) error {
-
+	_, err := ss.db.ExecContext(ctx, `INSERT INTO tasks
+	(id, title,	description, status, created_at, updated_at) 
+	values(?, ?, ?, ?, ?, ?)`, t.ID, t.Title, t.Description, t.Status,
+		t.CreatedAt, t.UpdatedAt)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (ss *SQLiteStore) UpdateTaskStatus(ctx context.Context, id uuid.UUID, s Status) error {
-
+	_, err := ss.db.ExecContext(ctx, `UPDATE tasks SET
+	status = ?, updated_at = ? WHERE id = ? `, s, time.Now(), id)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (ss *SQLiteStore) DeleteTask(ctx context.Context, id uuid.UUID) error {
-
+	_, err := ss.db.ExecContext(ctx, `DELETE FROM tasks WHERE id = ?`, id)
+	if err != nil {
+		return err
+	}
+	return nil
 }
