d1b1a55ded496864b38c93dd4f7e249ecb3ca5a9
Saswata Gorai <13601653+1saswata@users.noreply.github.com>
2026-07-14 21:48:21 +0530

    ref: refactor column.go

--- internal/ui/column.go
+++ internal/ui/column.go
@@ -1,7 +1,10 @@
 package ui
 
 import (
+	"context"
+
 	"charm.land/bubbles/v2/list"
+	tea "charm.land/bubbletea/v2"
 	"github.com/1saswata/kanban-tui-go/internal/kanban"
 )
 
@@ -19,4 +22,32 @@ func NewColumn(status kanban.Status) Column {
 	l.SetShowStatusBar(false)
 	l.SetShowHelp(false)
 	return Column{list: l, status: status}
+}
+
+type tasksLoadedMsg []kanban.Task
+
+type taskItem struct {
+	task kanban.Task
+}
+
+func (t taskItem) Title() string {
+	return t.task.Title
+}
+
+func (t taskItem) Description() string {
+	return t.task.Description
+}
+
+func (t taskItem) FilterValue() string {
+	return ""
+}
+
+func fetchTasks(store kanban.TaskStore) tea.Cmd {
+	return func() tea.Msg {
+		t, err := store.GetTasks(context.Background())
+		if err != nil {
+			return errMsg(err)
+		}
+		return tasksLoadedMsg(t)
+	}
 }
